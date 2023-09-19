80a6daacb48df6e2a889627aac9637c313841e89
SeverinaZheng <yunjia.zheng@mail.mcgill.ca>
2023-09-19 11:51:29 +0000

    adding some result

--- benchmarks/DMon_filter/scripts/filter_target_function.sh
+++ benchmarks/DMon_filter/scripts/filter_target_function.sh
@@ -1,6 +1,6 @@
 # TARGET="spin_lock"
 TARGET="down_write"
 TARGET_D="/$TARGET/d"
-PERF_DATA="/home/syncord/SynCord/benchmarks/will-it-scale/will_perf/mmap1_perf.data"
+PERF_DATA="/home/syncord/SynCord/benchmarks/will-it-scale/will_perf/lock1_perf.data"
 /usr/lib/linux-tools-5.4.0-107/perf script -i $PERF_DATA| grep -A 1 $TARGET| sed $TARGET_D| sed '/--/d'> result.txt
 awk '{print $2}' result.txt | sort | uniq > contend_func.txt
